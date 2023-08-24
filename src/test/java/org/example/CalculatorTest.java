738f51520d8041a0768a32e77249dcb48d8fbe8f
Kang Tae Sung <potensun@gmail.com>
2023-08-24 16:45:41 +0900

    계산기 코드 추가, 테스트 코드 추가

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+    public void testAddTrue() throws Exception {
+        Calculator calc = new Calculator();
+        int actualValue = calc.add(5, 10);
+        int expectedValue = 15;
+
+        assertTrue(actualValue == expectedValue);
+    }
+    public void testAddFalse() throws Exception {
+        Calculator calc = new Calculator();
+        int actualValue = calc.add(5, 10);
+        int expectedValue = 25;
+
+        assertFalse(actualValue != expectedValue);
+    }
+
+    public void testAddException() {
+        Calculator calc = new Calculator();
+        try  {
+            int result = calc.add(15, 20);
+            assertTrue(false);
+        } catch (Exception ex) {
+            assertTrue(true);
+        }
+    }
+//        if (actualValue == expectedValue) {
+//            assertTrue(true);
+//        } else {
+//            assertFalse(false);
+//        }
+
+}
